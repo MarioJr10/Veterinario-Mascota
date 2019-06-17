1f1682b972ddcc4975d256b2661840ef879c0d69
LeguiA <LeguiA@192.168.1.20>
2019-06-17 17:34:39 -0500

    Veterinario Mascota Eliminar

---
+++ src/java/Prueba/Ejercicio3.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Prueba;
+
+import dao.MascotaDao;
+import entidades.Mascota;
+import org.hibernate.Session;
+import utilitarios.HibernateUtil;
+
+/**
+ *
+ * @author LeguiA
+ */
+public class Ejercicio3 {
+
+    public static void main(String[] args) {
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        MascotaDao mascotadao = new MascotaDao();
+        Mascota actualizardatos = new Mascota(7, "Brunooo", "Jesus", "Chihuahua");
+        mascotadao.actualizarMascota(session, actualizardatos);
+
+        session.beginTransaction();
+        session.saveOrUpdate(actualizardatos);
+        session.getTransaction().commit();
+        session.close();
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP LOGUIN</title>
+    </head>
+    <body>
+    <center>
+        <h4>INICIAR SESSION!</h4>
+        <input type="text" name="usuario"><br><br>
+        <input type="password" name="contrasena"><br><br>
+        <input type="button" name="Aceptar" value="Aceptar" >
+    </center>
+</body>
+</html>
